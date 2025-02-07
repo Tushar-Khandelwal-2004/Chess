7f27ac5e4828ba442c5fb24a4c76b5bf495a7187
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2025-02-07 22:48:01 +0530

    Initialised Frontend Project

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import Landing from './pages/Landing';
+import Game from './pages/Game';
+
+function App() {
+
+  return (
+   <Router>
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/game' element={<Game/>}/>
+      </Routes>
+   </Router>
+  )
+}
+
+export default App
---
+++ Frontend/src/pages/Landing.tsx
@@ -0,0 +1,8 @@
+
+function Landing() {
+  return (
+    <div className="bg-red-200">Landing</div>
+  )
+}
+
+export default Landing
